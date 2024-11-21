4d8104684301d1c542b629b44c9e83131e6054ad
José Brito <josealbritose@gmail.com>
2024-11-21 00:30:55 -0300

    Connection with mongodb

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
 import app from "./app";
+import { connectDB } from './config/db';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
   console.log(`Server on port ${PORT}`)
+  connectDB();
 })
