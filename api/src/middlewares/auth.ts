f92bf6d8e9abdb64dc95cb2e6042a1a4aa8661d1
José Brito <josealbritose@gmail.com>
2024-11-21 01:13:01 -0300

    Middleware

---
+++ api/src/middlewares/auth.ts
@@ -0,0 +1,13 @@
+import type { Request,Response,NextFunction } from "express";
+import { authSchema } from "../schemas/user.schema";
+
+export const createAccountValidationMiddleware = (req:Request,res:Response,next:NextFunction) => {
+  try {
+    const body = req.body;
+    const result = authSchema.safeParse(body);
+    if(!result.success) throw new Error(JSON.stringify(result.error))
+    next();
+  } catch (error:any) {
+    return res.status(400).json({error:JSON.parse(error.message)})
+  }
+};
