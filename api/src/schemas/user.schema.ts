c7ad605c181ef3a1f777e8f72a39e727acd79d54
José Brito <josealbritose@gmail.com>
2024-11-21 01:12:32 -0300

    Zod AuthSchema

---
+++ api/src/schemas/user.schema.ts
@@ -0,0 +1,18 @@
+import z from 'zod';
+
+const email = z.string().email({message:'Invalid format email'});
+const fullName = z.string().min(4,{message:'The name is so short'});
+const password = z.string().min(6,{message:'The password must have min 6 characters'});
+const profilePic = z.string().optional();
+const lastConnection = z.date().nullable().default(null);
+
+export const authSchema = z.object({
+  email,
+  fullName,
+  password,
+  profilePic,
+  lastConnection
+});
+
+export type AuthSignUp = z.infer<typeof authSchema>;
+export type AuthSignIn = Pick<AuthSignUp,'email' | 'password'>;
