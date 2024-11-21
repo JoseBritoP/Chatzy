4ba8197776bb21b3979df70c0cde7900d8c57ce1
José Brito <josealbritose@gmail.com>
2024-11-21 06:25:29 -0300

    SignUP Handler Functionality

--- api/src/handlers/auth/auth.functions.ts
+++ api/src/handlers/auth/auth.functions.ts
@@ -1,15 +1,25 @@
 import type { Request, Response } from "express";
+import { signUp } from "../../controllers/auth/01 - signUp";
+import { generateJWT } from "../../utils/jwt";
 
-export const GETAUTH =  (req: Request, res: Response) => {
+export const GETAUTH = (req: Request, res: Response) => {
   return res.json({ DIY: "GET Auth" });
 };
 
-export const SIGNIN =  (req: Request, res: Response) => {
+export const SIGNIN = (req: Request, res: Response) => {
   return res.json({ DIY: "Sign in" });
 };
 
-export const SIGNUP =  (req: Request, res: Response) => {
-  return res.json({ DIY: "Sign up" });
+export const SIGNUP = async (req: Request, res: Response) => {
+  const data = req.body;
+  try {
+    const newAccount = await signUp(data);
+    generateJWT(newAccount.account, res);
+    return res.status(201).json(newAccount);
+  } catch (error: any) {
+    console.log('Error in signup controller',error.message)
+    return res.status(400).json({ error: error.message });
+  }
 };
 
 export const LOGOUT = (req: Request, res: Response) => {
