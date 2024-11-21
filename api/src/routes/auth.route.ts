6110ac909dac2653f4dc3c91c9f3d879213fa226
José Brito <josealbritose@gmail.com>
2024-11-21 01:12:53 -0300

    createAccountValidationMiddleware in use

--- api/src/routes/auth.route.ts
+++ api/src/routes/auth.route.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { AuthHandler } from "../handlers/auth/Auth";
 
+// Middlewares
+import { createAccountValidationMiddleware } from "../middlewares/auth";
+
 const authRouter = Router();
 
 // Endpoints
 
 authRouter.get("/", AuthHandler.GETAUTH);
-authRouter.post("/sign-up", AuthHandler.SIGNIN);
-authRouter.post("/sign-in", AuthHandler.SIGNUP);
+authRouter.post("/sign-up",createAccountValidationMiddleware, AuthHandler.SIGNUP);
+authRouter.post("/sign-in", AuthHandler.SIGNIN);
 authRouter.post("/logout", AuthHandler.LOGOUT);
 
 //
