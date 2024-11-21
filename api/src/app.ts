ec47c0479f41a7e73cb4eda25ce14692120db415
José Brito <josealbritose@gmail.com>
2024-11-21 16:32:27 -0300

    cookieparser middleware in use

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 import morgan from 'morgan'
+import cookieparser from 'cookie-parser'
 import mainRouter from './routes';
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
+app.use(cookieparser())
 
 app.use('/', mainRouter );
 
