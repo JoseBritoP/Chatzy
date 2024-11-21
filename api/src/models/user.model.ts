a297fb28c2c7f2f53e1b1867a363b963ac223b81
José Brito <josealbritose@gmail.com>
2024-11-21 00:53:37 -0300

    UserModel

---
+++ api/src/models/user.model.ts
@@ -0,0 +1,45 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+export interface User extends Document {
+  email: string;
+  fullName: string;
+  password: string;
+  profilePîc?: string;
+  lastConnection?: Date;
+}
+
+const UserSchema: Schema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLenght:6
+    },
+    profilePic: {
+      type: String,
+      required: false,
+      default:''
+    },
+    lastConnection: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model<User>('User',UserSchema);
+
+export default User;
