8d3bf5c9f4d7c7c378f4da384f7e19d2863a5747
José Brito <josealbritose@gmail.com>
2024-11-22 05:40:37 -0300

    getMessages Handler

--- api/src/handlers/message/message.functions.ts
+++ api/src/handlers/message/message.functions.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import { getUsersToChat } from "../../controllers/message/01 - getUsers";
 import AppError from "../../utils/appError";
+import { getMessages } from "../../controllers/message/02 - getMessages";
 
 export const GETUSERS = async (req: Request, res: Response) => {
   const loggedUser = req.user;
@@ -9,6 +10,19 @@ export const GETUSERS = async (req: Request, res: Response) => {
     return res.status(200).json(users);
   } catch (error:any) {
     console.log("Error in get users handler", error.message);
+    if(error instanceof AppError) return res.status(error.statusCode).json({error:error.message})
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const GETMESSAGES = async (req:Request,res:Response) => {
+  const { id:reciverId} = req.params;
+  const senderId = req.user._id;
+  try {
+    const messages = await getMessages({senderId,reciverId});
+    return res.status(200).json(messages);
+  } catch (error:any) {
+    console.log("Error in get messages handler", error.message);
     if(error instanceof AppError) return res.status(error.statusCode).json({error:error.message})
     return res.status(500).json({ error: 'Internal server error' });
   }
