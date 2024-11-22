f1eb5d7b16b6289479c4a79947a87024af5e4716
José Brito <josealbritose@gmail.com>
2024-11-22 07:35:02 -0300

    Page folders and Page init

---
+++ client/src/pages/index.ts
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import ProfilePage from "./ProfilePage";
+import SignInPage from "./SignInPage";
+import SettingsPage from "./SettingsPage";
+import SignUpPage from "./SignUpPage";
+
+export { HomePage, ProfilePage, SettingsPage, SignInPage, SignUpPage };
