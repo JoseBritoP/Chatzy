5dda6eecda30cc024261700db3006ca7f44da82c
José Brito <josealbritose@gmail.com>
2024-11-22 07:35:18 -0300

    Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { HomePage, ProfilePage, SettingsPage, SignInPage, SignUpPage } from "./pages";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/signin" element={<SignInPage/>}/>
+        <Route path="/settings" element={<SettingsPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+      </Routes>
     </div>
-  )
+  );
 }
